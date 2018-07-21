ce8523699da8ca78e1cc1fbfb335414fc6fb0f17
Vicente Soriano <vicente.soriano@icloud.com>
2018-07-21 18:19:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,9 +15,7 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-  var lowercase = 'hello', uppercase = 'HELLO'
- 
-  if (lowercase.toLowerCase() === lowercase) {
+  if (string.toLowerCase() === lowercase) {
     return "I can't hear you!"
   } else if (uppercase.toUpperCase() === uppercase) {
     return "YES INDEED!"
